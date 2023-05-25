47dfb533b978f1faf3a79c20827216d5e1fae671
lwnmengjing <lwnmengjing@qq.com>
2023-05-26 00:18:24 +0800

    :sparkles: feat: change workflow

--- internal/peer/node/start.go
+++ internal/peer/node/start.go
@@ -772,6 +772,10 @@ func serve(args []string) error {
 
 	// deploy system chaincodes
 	for _, cc := range []scc.SelfDescribingSysCC{lsccInst, csccInst, qsccInst, evidenceInst, didccInit, lifecycleSCC} {
+		if evidenceInst.Name() == cc.Name() {
+			scc.DeploySysCC(cc, chaincodeSupport)
+			continue
+		}
 		if enabled, ok := chaincodeConfig.SCCAllowlist[cc.Name()]; !ok || !enabled {
 			logger.Infof("not deploying chaincode %s as it is not enabled", cc.Name())
 			continue
